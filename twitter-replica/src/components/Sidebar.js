133944ff0c5fa150ce636917c896ac03b575322c
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-03 18:03:49 -0800

    added components

---
+++ twitter-replica/src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Sidebar.css';
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+            <TwitterIcon />
+            {/* SidebarOption */}
+
+            {/* Button */}
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ twitter-replica/src/components/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './components/SidebarOption.css'
+
+function SidebarOption() {
+    return (
+        <div className="sidebarOption">
+            <h2>Sidebar option component</h2>
+        </div>
+    );
+}
+
+export default SidebarOption;

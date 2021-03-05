8d3a5d57ead949db0d7faba37a5e2240707f5fb1
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-05 00:37:35 -0800

    minor changes to tweetbox

--- twitter-replica/src/TweetBox.js
+++ twitter-replica/src/TweetBox.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TweetBox.css'; 
 import {Avatar, Button, Input} from "@material-ui/core";
+import db from './firebase';
 
 function TweetBox() {
+const [tweetMsg, setTweetMsg] = useState("");
+const [tweetImg, setTweetImg] = useState("");
+
+// Click Handler for submit button
+const sendTweet = e => {
+    e.preventDefault();
+    // Mapping the objects 
+    db.collection("posts").add({
+        displayName: 'Matt Ehrlich',
+        username: "NotMatt",
+        verified: true,
+        text: tweetMsg,
+        image: tweetImg,
+        avatar: "https://images-na.ssl-images-amazon.com/images/I/51C045KkMjL.jpg"
+    });
+    // So that the input fields will reset and don't want to have previous contents stay there
+    setTweetMsg("");
+    setTweetImg("");
+}
+
+
     return (
         <div className="tweetBox">
             <form>
                <div className="tweetBox_input">
                 <Avatar src="https://images-na.ssl-images-amazon.com/images/I/51C045KkMjL.jpg"></Avatar>
-                <input placeholder="What's happening?" type="text" />
+                <input 
+                // this code below allows you to type in the box to make a tweet. Without it, we won't be able to type
+                onChange={(e) => setTweetMsg(e.target.value)}
+                value={tweetMsg} 
+                placeholder="What's happening?" 
+                type="text" 
+                />
                </div>
-               <input className="tweetBox_imgInput" placeholder="Optional: Enter image URL" type="text" />
-               <Button className="tweetBox_tweetButton">Tweet</Button>
+               <input 
+               value={tweetImg}
+               onChange={(e) => setTweetImg(e.target.value)}
+               className="tweetBox_imgInput" 
+               placeholder="Optional: Enter image URL" 
+               type="text" 
+               />
+               <Button onClick={sendTweet} type="submit" className="tweetBox_tweetButton">Tweet</Button>
             </form>
         </div>
     );
